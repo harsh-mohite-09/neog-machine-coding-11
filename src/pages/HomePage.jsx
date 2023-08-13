3ee8b5912a145d15c53b7272dd0a6a5e4c04e8ae
Harsh Mohite <harsh.mohite009@gmail.com>
2023-08-13 09:13:06 +0530

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <Heading textAlign="center" mt={8}>
+      MCR - 11
+    </Heading>
+  );
+};
+
+export default HomePage;
